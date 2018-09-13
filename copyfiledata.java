8c722b24988b13978e54883bbb6bf5ebe93f1f95
rajan verma <shivanshawesome31@gmail.com>
2018-09-13 13:12:43 +0530

    first commit

---
+++ copyfiledata.java
@@ -0,0 +1,21 @@
+import java .io.*;
+import java.io.IOException;
+class copyfiledata
+{
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedWriter bw=new BufferedWriter(new FileWriter("file1.txt"));
+		bw.write ("this is a file and my name is pushkar");
+		bw.close();
+		InputStream in=new FileInputStream(new File("file1.txt"));
+		OutputStream out=new FileOutputStream(new File("file 2.txt"));
+		byte[] buf=new byte[1024];
+		int len;
+		while((len=in.read(buf))>0)
+		{
+			out.write(buf,0,len);
+		}
+		in.close();
+		out.close();
+	}
+}
